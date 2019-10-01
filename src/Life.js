0fc2715b7888609a5a9dbecaf4a7e2c8bd3096ad
czeqin <czeqin@cyberway.net.cn>
2019-10-01 16:34:09 +0800

    安装相关包

---
+++ src/Life.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+import './life.less'
+class Life extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  
+            <div>测试测试</div>
+        );
+    }
+}
+ 
+export default Life;
